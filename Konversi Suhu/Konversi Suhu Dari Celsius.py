180b3aa1e1e34b132238c042a2d5198f97f7698e
Welly828 <ewal78503@gmail.com>
2026-08-11 11:10:01 +0700

    Implement temperature conversion from Celsius
    Added temperature conversion from Celsius to Reamur, Fahrenheit, and Kelvin.

--- Konversi Suhu/Konversi Suhu Dari Celsius.py
+++ Konversi Suhu/Konversi Suhu Dari Celsius.py
@@ -1,1 +1,16 @@
+#Konversi Suhu dari Celsius ke Satuan Lainnya.
 
+celsius = float(input("Masukan Suhu Dalam Celsius:"))
+print("--------------------------------------")
+print("Suhu Adalah:", celsius, "Celsius")
+
+reamur = (4/5) * celsius
+print("Suhu dalam Reamur Adalah:", reamur, "Reamur")
+
+fahrenheit = (9/5 * celsius) + 32
+print("Suhu dalam Fahrenheit:", fahrenheit, "Fahrenheit")
+
+kelvin = celsius + 273
+print("Suhu dalam Kelvin", kelvin, "Kelvin")
+
+print("--------------------------------------")
