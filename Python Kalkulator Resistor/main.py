6b906247cf2ea24740ad128f89f13f875792827f
Welly828 <ewal78503@gmail.com>
2026-08-21 22:31:52 +0700

    Add resistor color code calculator functionality
    Implement basic resistor color calculator with input prompts.

---
+++ Python Kalkulator Resistor/main.py
@@ -0,0 +1,33 @@
+Warna_Gelang = {
+    "hitam" : 0,
+    "coklat" : 1,
+    "merah" : 2,
+    "oranye" : 3,
+    "kuning" : 4,
+    "hijau" : 5,
+    "biru" : 6,
+    "violet" : 7,
+    "abu-abu" : 8,
+    "putih" : 9
+}
+
+Warna_Gelang_Pengali = {
+    "silver" : 0.01,
+    "emas" : 0.1,
+    "hitam" : 1,
+    "coklat" : 10,
+    "merah" : 100,
+    "oranye" : 1000,
+    "kuning" : 10000,
+    "hijau" : 100000,
+    "biru" : 1000000,
+    "violet" : 10000000,
+    "abu-abu" : 100000000,
+    "putih" : 1000000000
+}
+
+print(=== Kalkulator Resistor 4 Gelang ===)
+
+Gelang1 = input("Masukkan Warna Gelang 1 = ").lower()
+Gelang2 = input("Masukkan Warna Gelang 2 = ").lower()
+Gelang3 = input("Masukkan Warna Gelang 3 (Pengali) = ").lower()
